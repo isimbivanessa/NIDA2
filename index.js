624f8daa0c3eda66510fe5930f533f3c88e03896
brulyn <higirobru@gmail.com>
2019-09-24 17:17:40 +0200

    done

---
+++ index.js
@@ -0,0 +1,30 @@
+var cors = require("cors");
+var router = require("express")();
+
+var corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+var wsdl = "http://192.168.50.20/kyc/KYCService.asmx?wsdl";
+var controller = require("rest-to-soap-mapper");
+
+router.get(
+  "/",
+  cors(corsOptions),
+  controller(wsdl, "AuthenticateDocument", setArgs)
+);
+
+function setArgs(req) {
+  return {
+    documentNumber: req.query.doc,
+    BankCode: req.query.code
+  };
+}
+
+// let PORT = process.env.PORT ? process.env.PORT : 3500;
+let PORT = 3500;
+
+router.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
