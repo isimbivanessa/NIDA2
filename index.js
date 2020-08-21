2607798118038c94c0617933ea7a4e82601d32a2
brulyn <higirobru@gmail.com>
2020-08-21 11:26:00 +0200

    some changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var router = require("express")();
 
 var corsOptions = {
   origin: "*",
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 var wsdl = "http://192.168.50.20/kyc/KYCService.asmx?wsdl";
@@ -12,11 +12,14 @@ var controller = require("rest-to-soap-mapper");
 function setArgs(req) {
   return {
     documentNumber: req.query.doc,
-    BankCode: req.query.code
+    BankCode: req.query.code,
   };
 }
 
 router.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
   res.send("Nida node app");
 });
 
